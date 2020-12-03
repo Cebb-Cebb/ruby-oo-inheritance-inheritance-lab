daceb01ce32436bc06ca35715f7f4dc1da17bae5
user <github email address>
2020-12-03 10:03:03 -0500

    commit tree ba38e1e14f7485effbd65ec9622960879ba08681

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User 
+
+    attr_accessor :knowledge
+
+    def initialize
+        self.knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end 
+
+    # def knowledge
+    #     @knowledge
+    # end
 end
